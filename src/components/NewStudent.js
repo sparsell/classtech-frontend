c38d6e58326287e3fbfe3fdebad59fc394d69af2
Sonja Parsell <hello@sparsell.com>
2021-10-04 14:43:15 -0400

    created NewStudent component

---
+++ src/components/NewStudent.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class NewStudent extends React.Component {
+    render() {
+        return (
+         <div>
+                <p>Student's Name:</p>
+                <p>Student's Grade:</p>
+            </div>
+            )
+        }
+    }
+
+    export default NewStudent
