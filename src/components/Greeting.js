44166b2976ddb3c4090a44440e7d9fff35cb8895
Sonja Parsell <hello@sparsell.com>
2021-10-04 14:43:03 -0400

    created Greeting component

---
+++ src/components/Greeting.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Greeting extends React.Component {
+
+    render() {
+        return (
+            <h2>Welcome to ClassTech</h2>
+        )
+    }
+}
+
+
+
+export default Greeting
