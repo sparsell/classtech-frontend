aac51925a16793a0678d621ece9d0e1d9ac00331
Sonja Parsell <hello@sparsell.com>
2021-10-04 14:43:40 -0400

    created SchoolFilter component

---
+++ src/components/SchoolFilter.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+class SchoolFilter extends React.Component {
+
+    render() {
+        return (
+            <div className="school filter">
+                <h3>Choose your school</h3>
+                    <div className="school field">
+                        <select name="type"
+                                id="type">
+                            <option>Parkway</option>
+                            <option>Glenville</option>
+                            <option>Whitby</option>
+                            <option>Riverside</option>
+                            <option>Western Middle</option>
+                            <option>Central Middle</option>
+                            <option>Eastern Middle</option>
+                            <option>GCDS</option>
+                            <option>Sacred Heart</option>
+                        </select>
+                    </div>
+            </div>
+            )
+        }
+    }
+
+
+
+
+
+export default SchoolFilter
