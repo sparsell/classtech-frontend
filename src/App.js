8f8b7bc5af61383bea2887f9a21c041d9ef759a6
Sonja Parsell <hello@sparsell.com>
2021-10-07 13:53:37 -0400

    converted function to class

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
 import './App.css';
 import './index.css'
+import React from 'react'
 import Greeting from './components/Greeting';
 import SearchExisting from './components/SearchExisting'
-import NewStudent from './components/NewStudent'
+import NewStudentForm from './components/NewStudentForm'
 
-function App() {
-  return (
-    <div className="App">
-       
+class App extends React.Component {
+    render() {
+        return (
+            <div className="App">
             <div className="wrapper">
                 <Greeting />
             </div>
-
             <div>
                 <SearchExisting />
             </div>
-        
-
         <h2>--- OR ---</h2>
-        
             <div>
-                <NewStudent />
+                <NewStudentForm />
             </div>
-      
     </div>
     
-  );
+  )
+    }
+  ;
 }
 
 export default App;
