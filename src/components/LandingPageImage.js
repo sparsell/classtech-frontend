db4ead56190de494dc5c975ffb677d6e89e5a197
Sonja Parsell <hello@sparsell.com>
2021-10-12 15:49:22 -0400

    added LandingPageImage component

---
+++ src/components/LandingPageImage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const LandingPageImage = { 
+
+    <div
+        class="bg-image"
+        style="
+        background-image: url('./public/kids_with_phone');
+        height: 100vh;">
+        
+    </div>
+}   
+
+export default LandingPageImage
