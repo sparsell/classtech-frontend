9e20eba2b31682e45cda1a6fea108f4907fd13ff
Sonja Parsell <hello@sparsell.com>
2021-10-12 15:49:06 -0400

    tidy up imports

--- src/App.js
+++ src/App.js
@@ -2,28 +2,39 @@ import './App.css';
 import './index.css'
 import React from 'react'
 import Greeting from './components/Greeting';
-import SearchExisting from './components/SearchExisting'
-import NewStudentForm from './components/NewStudentForm'
+import Button from 'react-bootstrap/Button'
+
 
 class App extends React.Component {
+
+    
+
+    componentDidMount() {
+        fetch('http://localhost:3000/api/v1/schools/5/grades/4/students')
+            .then(res => res.json())
+            .then(data => console.log(data))
+        }
+
     render() {
         return (
-            <div className="App">
-            <div className="wrapper">
-                <Greeting />
-            </div>
             <div>
-                <SearchExisting />
+                <div className="App">
+                    <br></br>
+                    {/* <NavBar /> */}
+                    <Greeting />
+                    <br></br>
+                    <br></br>
+                    <br></br>
+                    <Button variant="outline-secondary" size="lg">
+                    Add your student
+                    </Button>
+                    <Button variant="outline-secondary" size="lg">
+                    See the results
+                    </Button>
+                </div>
             </div>
-        <h2>--- OR ---</h2>
-            <div>
-                <NewStudentForm />
-            </div>
-    </div>
-    
-  )
+        ) 
     }
-  ;
 }
 
 export default App;
