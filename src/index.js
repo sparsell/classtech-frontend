8025581869fdf51491f330601a3ed8444ee9d8f9
Sonja Parsell <hello@sparsell.com>
2021-10-15 17:00:09 -0400

    added store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 import reportWebVitals from './reportWebVitals';
+import gradeReducer from './reducers/gradeReducer.js'
+
+import App from './App';
+
+
+// set up store
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+
+let myStore = createStore(gradeReducer, composeEnhancers(applyMiddleware(thunk)))
 
 ReactDOM.render(
-  <React.StrictMode>
+
+<Provider store={myStore}> 
     <App />
-  </React.StrictMode>,
+</Provider>
+    ,
   document.getElementById('root')
 );
 
