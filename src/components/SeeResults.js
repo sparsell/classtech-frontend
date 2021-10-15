1d6a099dbe45d108a52ef5d518bdff39e262db6b
Sonja Parsell <hello@sparsell.com>
2021-10-15 16:59:13 -0400

    new component

---
+++ src/components/SeeResults.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import SeeResultsForm from './SeeResultsForm'
+// import SchoolFilter from './SchoolFilter'
+// import GradeFilter from './GradeFilter'
+
+
+class SeeResults extends React.Component {
+
+    render() {
+        return (
+            <div className='action-container'>
+            <h3>See the Results</h3>
+            <SeeResultsForm />
+                   
+            </div>
+        )
+
+        }
+    }
+
+    export default SeeResults
