8e5416133118c885b483b3913ab659f15818f4dc
Sonja Parsell <hello@sparsell.com>
2021-10-15 16:55:15 -0400

    greeting className assigned

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -4,9 +4,9 @@ class Greeting extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="logo">
                 <h1>ClassTech</h1>
-                <h4>"No darling, not everyone in your class has a phone."</h4>
+                <p>"No darling, not everyone in your class has a phone."</p>
                 <p> - every parent</p>
             </div>
         )
