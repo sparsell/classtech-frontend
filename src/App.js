c5fe4a7077ec5f725f4b77ce38fa8a462e71a234
Sonja Parsell <hello@sparsell.com>
2021-10-15 16:57:54 -0400

    adding components to home page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
 import './index.css'
 import React from 'react'
+
 import Greeting from './components/Greeting';
-import Button from 'react-bootstrap/Button'
+import NewStudent from './components/NewStudent'
+import SeeResults from './components/SeeResults'
 
 
 class App extends React.Component {
@@ -10,7 +12,7 @@ class App extends React.Component {
     
 
     componentDidMount() {
-        fetch('http://localhost:3000/api/v1/schools/5/grades/4/students')
+        fetch('http://localhost:3000/api/v1/schools/10/grades/6/students')
             .then(res => res.json())
             .then(data => console.log(data))
         }
@@ -20,17 +22,11 @@ class App extends React.Component {
             <div>
                 <div className="App">
                     <br></br>
-                    {/* <NavBar /> */}
                     <Greeting />
-                    <br></br>
-                    <br></br>
-                    <br></br>
-                    <Button variant="outline-secondary" size="lg">
-                    Add your student
-                    </Button>
-                    <Button variant="outline-secondary" size="lg">
-                    See the results
-                    </Button>
+                   <div className="justify-content-md=center">
+                        <NewStudent />
+                        <SeeResults />
+                    </div>
                 </div>
             </div>
         ) 
