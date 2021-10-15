5d7eee76bcfdc7bda7613999c1411ba63f26cf53
Sonja Parsell <hello@sparsell.com>
2021-10-15 16:58:37 -0400

    wip school filter

--- src/components/SchoolFilter.js
+++ src/components/SchoolFilter.js
@@ -4,23 +4,19 @@ class SchoolFilter extends React.Component {
 
     render() {
         return (
-            <div className="school filter">
-                
+//             {this.props.categories((items , index)=>{
+//   <option key{index}>{items.categoryName} </option>
+// })}
+            <div className="block-example">
                 <h3>School:</h3>
-                    <div className="school field">
                         <select name="type"
                                 id="type">
                             <option>Parkway</option>
                             <option>Glenville</option>
-                            <option>Whitby</option>
-                            <option>Riverside</option>
                             <option>Western Middle</option>
                             <option>Central Middle</option>
                             <option>Eastern Middle</option>
-                            <option>GCDS</option>
-                            <option>Sacred Heart</option>
                         </select>
-                    </div>
             </div>
             )
         }
