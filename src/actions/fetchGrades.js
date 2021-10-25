21cbce319c36e0930b63066e28bd206b9b45ed9a
Sonja Parsell <hello@sparsell.com>
2021-10-25 12:30:09 -0400

    wip fetch grades

---
+++ src/actions/fetchGrades.js
@@ -0,0 +1,7 @@
+export function fetchGrades(action) {
+    return action
+    // fetch('http://localhost:3000/api/v1/grades/4/students')
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+
+}
