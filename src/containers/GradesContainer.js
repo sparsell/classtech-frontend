a6f97535783b284f757e316609a1f6ff98c02ac1
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:33:30 -0400

    created GradesContainer.js

---
+++ src/containers/GradesContainer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import Grades from '../components/Grades.js'
+import { fetchGrades } from '../actions/fetchGrades.js'
+
+class GradesContainer extends React.Component {
+
+    componentDidMount() {
+            this.props.fetchGrades()
+        }
+
+    render() {
+        return (
+            <div>
+            GradesContainer Component
+            <Grades grades={this.props.grades}/>
+            </div>
+        )
+    }
+}
+
+ const mapStateToProps = state => {
+        return {
+            grades: state.grades
+        }
+    }
+
+export default connect(mapStateToProps, {fetchGrades})(GradesContainer)
