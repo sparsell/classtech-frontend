af5dcf76ac876911fa83bd2a490d8ad664359bc5
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:34:29 -0400

    added switch statement

--- src/reducers/gradeReducer.js
+++ src/reducers/gradeReducer.js
@@ -2,5 +2,10 @@
 
 export default function gradeReducer(state = {grades: []}, action) {
 
+    switch (action.type) {
+        case "FETCH_GRADES":
+            return state
+       default:
     return state
+    }
 }
