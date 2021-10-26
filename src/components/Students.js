ff8a81ccb48f97c4b29b914a8f4a261d42ba5399
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:33:16 -0400

    created Students.js

---
+++ src/components/Students.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Students extends React.Component {
+    render() {
+    return (
+        <div>
+        Students
+        </div>
+        )
+    }
+}
+
+export default Students
