3d28c9e94c52bfd9dae6fcd1730893ad26128afe
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:40:55 -0400

    testing student index rendering

--- src/components/NewStudent.js
+++ src/components/NewStudent.js
@@ -1,14 +1,29 @@
 import React from 'react'
+// import { connect } from 'react-redux'
 import NewStudentForm from './NewStudentForm'
+import Students from './Students'
+// import { fetchStudents } from '../actions/fetchStudents'
 
-class NewStudent extends React.Component {
-    render() {
+    class NewStudent extends React.Component {
+
+        render() {
         return (
+            
          <div className="action-container">
+         NewStudent Component
             <NewStudentForm />
+            <Students />
         </div>
             )
-        }
     }
+}
+
+    // const mapStateToProps = state => {
+    //     return {
+    //         students: state.students
+    //     }
+    // }
+
+    // export default connect(mapStateToProps, {fetchStudents})(NewStudent)
 
     export default NewStudent
