2a8a389742882fed762a5e3ec3b5fd432f6ef984
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:38:28 -0400

    wip testing fetch

--- src/actions/fetchGrades.js
+++ src/actions/fetchGrades.js
@@ -1,7 +1,12 @@
-export function fetchGrades(action) {
-    return action
-    // fetch('http://localhost:3000/api/v1/grades/4/students')
-    //         .then(res => res.json())
-    //         .then(data => console.log(data))
 
+export function fetchGrades() {
+    return (dispatch) => {
+    fetch('http://localhost:3000/api/v1/grades')
+            .then(res => res.json())
+            // .then(data => console.log(data))
+            .then(grades => dispatch({
+                type: 'FETCH_GRADES',
+                payload: grades,
+            }))
+        }
 }
