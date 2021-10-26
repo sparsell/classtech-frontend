4254d9082e8a4f49f351f73b4b31acb8d04dd6a5
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:38:05 -0400

    moved componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,36 +1,32 @@
 import './App.css';
 import './index.css'
 import React from 'react'
-
-import Greeting from './components/Greeting';
-import NewStudent from './components/NewStudent'
-import SeeResults from './components/SeeResults'
-
+import { connect } from 'react-redux'
+import StudentsContainer from './containers/StudentsContainer.js'
+import GradesContainer from './containers/GradesContainer.js'
+import Greeting from './components/Greeting.js'
+import LandingPageImage from './components/LandingPageImage.js'
 
 class App extends React.Component {
 
-    
+    // componentDidMount() {
+        
+    //     }
 
-    componentDidMount() {
-        fetch('http://localhost:3000/api/v1/schools/10/grades/6/students')
-            .then(res => res.json())
-            .then(data => console.log(data))
-        }
-
-    render() {
-        return (
-            <div>
-                <div className="App">
-                    <br></br>
-                    <Greeting />
-                   <div className="justify-content-md=center">
-                        <NewStudent />
-                        <SeeResults />
+        render() {
+            return (
+                <div>
+                    <div className="App">
+                        <Greeting />
+                         <LandingPageImage />
+                        <div className="justify-content-md=center">
+                            <StudentsContainer />
+                            <GradesContainer />
+                        </div>
                     </div>
                 </div>
-            </div>
-        ) 
+            ) 
     }
 }
 
-export default App;
+export default connect()(App);
