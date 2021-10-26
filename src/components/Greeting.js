c45f24ce945ce92ec6bdb34a5cd0409dd61cd7c4
Sonja Parsell <hello@sparsell.com>
2021-10-26 19:39:19 -0400

    converted to functional component

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,18 +1,14 @@
 import React from 'react'
 
-class Greeting extends React.Component {
-
-    render() {
-        return (
+const Greeting = () => {
+    return (
             <div className="logo">
+                GreetingComponent
                 <h1>ClassTech</h1>
                 <p>"No darling, not everyone in your class has a phone."</p>
                 <p> - every parent</p>
             </div>
         )
-    }
 }
-
-
 
 export default Greeting
