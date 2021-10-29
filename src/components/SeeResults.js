20ef47503b5982a334f07d3a4d4d2b6fa603d43e
Sonja Parsell <hello@sparsell.com>
2021-10-29 17:08:57 -0400

    deleted unused code

--- src/components/SeeResults.js
+++ src/components/SeeResults.js
@@ -3,12 +3,14 @@ import SeeResultsForm from './SeeResultsForm'
 
     const SeeResults = () => {
         return (
-            <div className='action-container'>
-            SeeResults Component
-                <h3>View by Grade</h3>
-                <SeeResultsForm />   
+            <div>
+                <h2>View by Grade</h2>
+                    <SeeResultsForm />   
             </div>
         )
     }
 
     export default SeeResults
+
+
+  
