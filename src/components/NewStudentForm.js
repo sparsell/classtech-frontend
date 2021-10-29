3e64e63d67114497470ad4055e842a9d4c968ff2
Sonja Parsell <hello@sparsell.com>
2021-10-29 17:09:12 -0400

    wip new student form

--- src/components/NewStudentForm.js
+++ src/components/NewStudentForm.js
@@ -1,47 +1,137 @@
 import React from 'react'
-import GradeFilter from "./GradeFilter"
+import { connect } from 'react-redux'
+import { addStudent } from '../actions/addStudent.js'
 
 class NewStudentForm extends React.Component {
-
-    constructor(props) {
-        super(props)
-        this.state = {
+    
+    state = {
             name: '',
             school: '',
-            grade: ''
+            // grade_name: '', 
+            grade_id: '', 
+            has_phone: false,
+            has_other_device: false,
+            screen_time: ''
         }
-        this.handleChange = this.handleChange.bind(this)
-        this.handleSubmit = this.handleSubmit.bind(this)
-    }
+    
+        
+        handleChange = (event) => {
+            this.setState({
+                [event.target.name]: event.target.value,
+            })
+        }
 
-        handleChange(event) {
-            this.setState({name: event.target.value})
+        handleCheckbox = (event) => {
+            const target = event.target
+            const value = target.type === 'checkbox' ? target.checked : target.value
+            const name = target.name
+            this.setState({
+                 [name]: value
+            })
         }
-        handleSubmit(event) {
+
+        handleSubmit = (event) => {
             event.preventDefault()
+            this.props.addStudent(this.state)
         }
+
 
     render() {
         return (
-         <div className="new-student">
-         NewStudentForm
-              <h3>Add a student</h3>
-                <form onSubmit={this.handleSubmit}>
+         <div className="form-container">
+                <form className="ui form" onSubmit={this.handleSubmit}>
                     <label>Name:</label>
-                    <input type="text" value={this.state.value} />
-
-                    <label>School:</label>
-                    <input type="select" />
+                    <input 
+                        type="text" 
+                        placeholder="First Name"
+                        name="name" //has to match attribute name in state
+                        value={this.state.name} 
+                        onChange={this.handleChange}
+                    />
+                    <br></br>
+                    <br></br>
 
                     <label>Grade:</label>
-                    {/* <input type="select" /> */}
-                    <GradeFilter />
+                    <select 
+                    // value={this.state.grade_name}
+                    // name="grade_name"
+                    value={this.state.grade_id}
+                    name="grade_id"
+                    onChange={this.handleChange}>
+                        {/* <GradeFilter /> */}
+                    <option value="" disabled selected>Select grade</option>
+                        <option value="2">2nd Grade</option>
+                        <option value="3">3rd Grade</option>
+                        <option value="4">4th Grade</option>
+                        <option value="5">5th Grade</option>
+                        <option value="6">6th Grade</option>
+                        <option value="7">7th Grade</option>
+                        <option value="8">8th Grade</option>
+                    </select>
+                        
+                    <br></br>
+                    <br></br>
 
-                    <input type="submit"></input>
+                    <label>School:</label>
+                    <select 
+                        value={this.state.school} 
+                        name="school"
+                        onChange={this.handleChange}> 
+                            <option value="" disabled selected>Select school</option>
+                            <option>North Street</option>
+                            <option>Riverside</option>
+                            <option>Parkway</option>
+                            <option>Glenville</option>
+                            <option>Western Middle</option>
+                            <option>Central Middle</option>
+                            <option>Eastern Middle</option>
+                        </select>
+
+                    <br></br>
+                    <br></br>
+
+                    <label>
+                    My child has a phone (check if "yes"):
+                    <input
+                        name="has_phone"
+                        type="checkbox"
+                        // checked={this.state.has_phone} 
+                        value={this.state.has_phone}
+                        onChange={this.handleCheckbox} 
+                    />
+                  
+                    </label>
+
+                    <br></br>
+
+                    <label>My child has another / a different electronic device (check box if "yes"): </label>
+                    <input 
+                        type="checkbox" 
+                        name="has_other_device"
+                        value={this.state.has_other_device} 
+                        onChange={this.handleCheckbox}
+                    />
+                    <br></br>
+
+                    <label>What screen time limits (if any) does your child have?    </label>
+                    <input 
+                    type="textarea" 
+                        name="screen_time"
+                        value={this.state.screen_time} 
+                        onChange={this.handleChange}
+                    />
+                   <br></br>
+                   <br></br>
+
+                    <input 
+                        className="ui primary button" 
+                        type="submit"
+                    />
                 </form>
         </div>
             )
         }
     }
 
-export default NewStudentForm
+export default connect(null, {addStudent})(NewStudentForm)
+//alternative to mapDispatchToProps
