d146741163af85ce63627518a1165f3842e70f2c
Sonja Parsell <hello@sparsell.com>
2021-10-29 17:10:30 -0400

    wip router

--- src/App.js
+++ src/App.js
@@ -2,26 +2,24 @@ import './App.css';
 import './index.css'
 import React from 'react'
 import { connect } from 'react-redux'
-import StudentsContainer from './containers/StudentsContainer.js'
+import { Route } from 'react-router';
 import GradesContainer from './containers/GradesContainer.js'
 import Greeting from './components/Greeting.js'
 import LandingPageImage from './components/LandingPageImage.js'
+import HomePage from './components/HomePage.js'
 
 class App extends React.Component {
-
-    // componentDidMount() {
-        
-    //     }
 
         render() {
             return (
                 <div>
                     <div className="App">
+                        {/* <div className="logo" style={{backgroundImage: `url(${image}` }}></div> */}
                         <Greeting />
                          <LandingPageImage />
                         <div className="justify-content-md=center">
-                            <StudentsContainer />
                             <GradesContainer />
+                            <Route exact path='/' render={() => <HomePage />} />
                         </div>
                     </div>
                 </div>
