9749f2757ce167c6fa26cf83886dfa9667292f95
Sonja Parsell <hello@sparsell.com>
2021-10-29 17:02:43 -0400

    wip - fetch students

--- src/actions/fetchStudents.js
+++ src/actions/fetchStudents.js
@@ -2,10 +2,9 @@ export function fetchStudents() {
     return (dispatch) => {
     fetch('http://localhost:3000/api/v1/students')
             .then(res => res.json())
-            // .then(data => console.log(data))
             .then(students => dispatch({
                 type: 'FETCH_STUDENTS',
                 payload: students,
-            }))
+        }))
     }
 }
