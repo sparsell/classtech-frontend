055d758d5d25a1c40e8e8818d848654eace73956
Sonja Parsell <hello@sparsell.com>
2021-10-29 16:58:30 -0400

    grade fileter with grade_id values

--- src/components/GradeFilter.js
+++ src/components/GradeFilter.js
@@ -1,21 +1,39 @@
 import React from 'react'
 
-class GradeFilter extends React.Component {
-    render() {
-        return (
-            <div className="grade filter">
-                <select>
-                    <option>2nd Grade</option>
-                    <option>3rd Grade</option>
-                    <option>4th Grade</option>
-                    <option>5th Grade</option>
-                    <option>6th Grade</option>
-                    <option>7th Grade</option>
-                    <option>8th Grade</option>
-                </select>
-            </div>
-        )
-    }
+const GradeFilter = () => {
+    return (
+            <select>
+                <option value="" disabled selected>Select grade</option>
+                <option value="2">2nd Grade</option>
+                <option value="3">3rd Grade</option>
+                <option value="4">4th Grade</option>
+                <option value="5">5th Grade</option>
+                <option value="6">6th Grade</option>
+                <option value="7">7th Grade</option>
+                <option value="8">8th Grade</option>
+            </select>
+    )
 }
+
+// class GradeFilter extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 <form>
+//                     <select>
+//                         <option value="" disabled selected>Select grade</option>
+//                         <option>2nd Grade</option>
+//                         <option>3rd Grade</option>
+//                         <option>4th Grade</option>
+//                         <option>5th Grade</option>
+//                         <option>6th Grade</option>
+//                         <option>7th Grade</option>
+//                         <option>8th Grade</option>
+//                     </select>
+//                 </form>
+//             </div>
+//         )
+//     }
+// }
 
 export default GradeFilter
