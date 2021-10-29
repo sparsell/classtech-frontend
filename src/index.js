becbbae3ae55c191e424aa61396701833bf8842e
Sonja Parsell <hello@sparsell.com>
2021-10-29 16:57:51 -0400

    added react-router-dom

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
+import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux'
 import gradeReducer from './reducers/gradeReducer'
 import reportWebVitals from './reportWebVitals';
@@ -22,7 +23,9 @@ const store = createStore(
 ReactDOM.render(
 
 <Provider store={store}> 
-    <App />
+    <Router>
+        <App />
+    </Router>
 </Provider>
     ,
   document.getElementById('root')
