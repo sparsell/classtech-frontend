0af0fb89d2f4b19c5dcfa9d8629620e060e79e90
Sonja Parsell <hello@sparsell.com>
2021-10-29 16:54:06 -0400

    wip - adding image src

--- src/components/LandingPageImage.js
+++ src/components/LandingPageImage.js
@@ -2,11 +2,21 @@ import React from 'react'
 
 const LandingPageImage = () => { 
 
+    // const image  = ''
+
     return (
     <div>
-        Image
+    image here
+        <image src={'./images/kids_with_phone.jpg'} />
     </div>
     )
 }   
 
 export default LandingPageImage
+
+
+// const SampleComponent = () => (
+//   <div>
+//     <img src={require('./styles/images/deadline.png')} alt="Test" />
+//   </div>
+// );
