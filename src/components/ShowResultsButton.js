3d1efa3001dc55eaa97a2cefdea78ddade782dc0
Sonja Parsell <hello@sparsell.com>
2021-10-29 17:01:48 -0400

    created ShowResultsButton component

---
+++ src/components/ShowResultsButton.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ShowResultsButton = () => {
+    return (
+        <button className="ui primary button"> Show Me</button>
+    )
+}
+
+export default ShowResultsButton
