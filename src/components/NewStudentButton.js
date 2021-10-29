ad897a00009a5445c8b2b2ccab638274a6dae8e6
Sonja Parsell <hello@sparsell.com>
2021-10-29 17:01:06 -0400

    created NewStudentButton component

---
+++ src/components/NewStudentButton.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const NewStudentButton = () => {
+    return (
+        <button className="ui primary button"> Add a new Student</button>
+    )
+}
+
+export default NewStudentButton
