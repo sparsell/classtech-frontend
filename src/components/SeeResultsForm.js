b8e9147e58de44a234e12011bd5026df41bb828d
Sonja Parsell <hello@sparsell.com>
2021-10-31 17:45:10 -0400

    wip handleSubmit and handleChange

--- src/components/SeeResultsForm.js
+++ src/components/SeeResultsForm.js
@@ -1,6 +1,5 @@
 import React from 'react'
-// import SchoolFilter from './components/SchoolFilter'
-import GradeFilter from './GradeFilter'
+// import { connect } from 'react-redux'
 
 class SeeResultsForm extends React.Component {
     constructor() {
@@ -8,24 +7,60 @@ class SeeResultsForm extends React.Component {
         this.state = {
             name: '',
             school: '',
-            grade: ''
+            grade_id: ''
         }
     }
-        handleGradeSubmit(event) {
+
+        handleChange = (event) => {
+            this.setState({
+                [event.target.name]: event.target.value
+            })
+        }
+
+        handleSubmit = (event) => {
             event.preventDefault()
+            
         }
 
     render() {
         return (
-        <div>
-            <form onSubmit={this.handleSubmit}>
-            
-                {/* <label>School:</label>
-                    <input type="select" onChange={this.handleNameChange} value={this.state.value} /> */}
+        <div className="form-container">
+            <form onSubmit={this.handleSubmit} className="ui form" >
 
-                    <label>Grade:</label>
-                    <GradeFilter />
-                    <input type="submit"></input>
+                  <label>Grade:</label>
+                    <select 
+                        // value={this.state.grade_name}
+                        // name="grade_name"
+                        value={this.state.grade_id}
+                        name="grade_id"
+                        onChange={this.handleChange}>
+
+                        <option value="" disabled selected>Select Your Grade</option>
+                        <option value="2">2nd Grade</option>
+                        <option value="3">3rd Grade</option>
+                        <option value="4">4th Grade</option>
+                        <option value="5">5th Grade</option>
+                        <option value="6">6th Grade</option>
+                        <option value="7">7th Grade</option>
+                        <option value="8">8th Grade</option>
+                    </select>
+
+                    <label>School:</label>
+                    <select 
+                        value={this.state.school} 
+                        name="school"
+                        onChange={this.handleChange}> 
+                            <option value="" disabled selected>Select Your School</option>
+                            <option>North Street</option>
+                            <option>Riverside</option>
+                            <option>Parkway</option>
+                            <option>Glenville</option>
+                            <option>Western Middle</option>
+                            <option>Central Middle</option>
+                            <option>Eastern Middle</option>
+                        </select>
+                        <input className="ui primary button" type="submit"/>
+
             </form>        
         </div>
         )
