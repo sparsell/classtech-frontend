c76a511226e8d9e2907b5616725ef9890c4c4143
Sonja Parsell <hello@sparsell.com>
2021-10-31 17:43:33 -0400

    added Link to new student button

--- src/components/NewStudentButton.js
+++ src/components/NewStudentButton.js
@@ -1,9 +1,13 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+// import { Route } from 'react-router'
+// import NewStudentForm from './NewStudentForm'
 
 const NewStudentButton = () => {
     return (
-        <button className="ui primary button"> Add a new Student</button>
+        <div>
+            <Link to={'/students/new'}><button className="ui primary button"> Add a new Student</button></Link>
+        </div>
     )
 }
 
