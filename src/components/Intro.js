5e40ba8c02a7024a31d33fa135a72499ed12574d
Sonja Parsell <hello@sparsell.com>
2021-10-31 17:38:53 -0400

    added Intro component

---
+++ src/components/Intro.js
@@ -0,0 +1,11 @@
+// import React from react 
+
+const Intro = () => {
+    return (
+        <div className="intro">
+          <h3>We know, everyone in your class says they have a phone but do they really? How can you tell? </h3>
+        </div>
+    )
+}
+
+export default Intro
