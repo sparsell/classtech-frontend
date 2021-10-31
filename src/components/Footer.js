7b0160bd9dbb3619f11b4e8462cf2a5e819b8879
Sonja Parsell <hello@sparsell.com>
2021-10-31 17:38:44 -0400

    added Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <div className="footer">
+                ClassTech©2021
+            </div>
+        </div>
+    )
+}
+
+export default Footer
