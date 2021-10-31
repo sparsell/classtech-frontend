5a74680f88b7985e723916c7af811d233bfa689e
Sonja Parsell <hello@sparsell.com>
2021-10-31 17:46:05 -0400

    wip students fetch

--- src/reducers/studentReducer.js
+++ src/reducers/studentReducer.js
@@ -1,10 +1,20 @@
 // keeps track of all students
 
 export default function studentReducer(state = {students: []}, action) {
-    debugger
+    // debugger
+
     switch (action.type) {
-        case 'FETCH_STUDENTS':
-            return state 
+        // case 'ADD_STUDENT':
+        //     return state 
+        // case 'DELETE_STUDENT':
+        //     return state 
+        // case 'EDIT_STUDENT':
+        //     return state 
+        case "FETCH_STUDENTS":
+            return {
+                ...state,
+                students: action.payload
+            }
         default:
         return state
     }
