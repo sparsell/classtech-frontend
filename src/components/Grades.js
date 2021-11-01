019fcc44cccaa36a9baf35769994543c3e1dae3b
Sonja Parsell <hello@sparsell.com>
2021-11-01 10:08:36 -0400

    removed copy

--- src/components/Grades.js
+++ src/components/Grades.js
@@ -1,15 +1,13 @@
 import React from 'react'
 
 const Grades = (props) => {
-
+   
  console.log(props)
-
         return (
             <div>
-                Grades Component
                 {props.grades.map(grade => <ul key={grade.id}>{grade.grade_name}</ul>)}
             </div>
         )
-}
+    }
 
 export default Grades
