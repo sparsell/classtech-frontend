1358e4958a33f382e716eb9a7df9ee1446cb511c
Sonja Parsell <hello@sparsell.com>
2021-11-01 10:11:12 -0400

    adding cases to reducer

--- src/reducers/studentReducer.js
+++ src/reducers/studentReducer.js
@@ -4,17 +4,14 @@ export default function studentReducer(state = {students: []}, action) {
     // debugger
 
     switch (action.type) {
-        // case 'ADD_STUDENT':
-        //     return state 
-        // case 'DELETE_STUDENT':
-        //     return state 
-        // case 'EDIT_STUDENT':
-        //     return state 
+        case 'ADD_STUDENT':
+            return {...state, students: action.payload}
         case "FETCH_STUDENTS":
-            return {
-                ...state,
-                students: action.payload
-            }
+            return {...state, students: action.payload}
+        case 'DELETE_STUDENT':
+            return state 
+        case 'EDIT_STUDENT':
+            return state 
         default:
         return state
     }
