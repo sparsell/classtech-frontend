80a419ed36e7305ffd1a52fb2ddc083838089202
Sonja Parsell <hello@sparsell.com>
2021-11-01 10:03:23 -0400

    wip routing with button link

--- src/components/NewStudentButton.js
+++ src/components/NewStudentButton.js
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-// import { Route } from 'react-router'
-// import NewStudentForm from './NewStudentForm'
+import { Link, Route, Switch } from 'react-router-dom'
+import NewStudentForm from './NewStudentForm'
 
 const NewStudentButton = () => {
     return (
         <div>
             <Link to={'/students/new'}><button className="ui primary button"> Add a new Student</button></Link>
+            {/* <Route path="/students/new">
+            <NewStudentForm />
+          </Route> */}
+          {/* <Link to="/students/new" component={NewStudentForm} /> */}
+
+          <Switch>
+            <Route exact path={"/students/new"} component={NewStudentForm} />
+        </Switch>
+
         </div>
     )
 }
