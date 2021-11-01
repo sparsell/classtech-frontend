afce88d6ab06638f2d90e32b70ee0c94bd5b3986
Sonja Parsell <hello@sparsell.com>
2021-11-01 10:23:34 -0400

    added export default

--- src/actions/fetchGrades.js
+++ src/actions/fetchGrades.js
@@ -1,5 +1,5 @@
 
-export function fetchGrades() {
+export default function fetchGrades() {
     return (dispatch) => {
     fetch('http://localhost:3000/api/v1/grades')
             .then(res => res.json())
