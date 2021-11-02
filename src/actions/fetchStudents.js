86b47417ee24a5e96082f5d79084aa895d4d8b55
Sonja Parsell <hello@sparsell.com>
2021-11-02 06:14:43 -0400

    added default

--- src/actions/fetchStudents.js
+++ src/actions/fetchStudents.js
@@ -1,4 +1,4 @@
-export function fetchStudents() {
+export default function fetchStudents() {
     return (dispatch) => {
     fetch('http://localhost:3000/api/v1/students')
             .then(res => res.json())
