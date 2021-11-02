c95791b6bf34e244f9fa56a2762dd12aca5be769
Sonja Parsell <hello@sparsell.com>
2021-11-02 06:19:01 -0400

    wip - rendering students

--- src/containers/StudentsContainer.js
+++ src/containers/StudentsContainer.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import NewStudent from '../components/NewStudent.js'
-import SeeResults from '../components/SeeResults.js' 
-import { fetchStudents } from '../actions/fetchStudents.js'
-// import Students from '../components/Students.js'
+// import NewStudent from '../components/NewStudent.js'
+// import SeeResults from '../components/SeeResults.js' 
+import fetchStudents from '../actions/fetchStudents.js'
+import Students from '../components/Students.js'
 
 class StudentsContainer extends React.Component {
 
@@ -12,12 +12,12 @@ class StudentsContainer extends React.Component {
         }
 
     render() {
-        // console.log("Students container data", this.props.students)
+        console.log("Students container data", this.props.students)
         return (
             <div>
-                <NewStudent />
-                <SeeResults />
-                {/* <Students grades={this.props.students}/> */}
+                {/* <NewStudent />
+                <SeeResults /> */}
+                <Students students={this.props.students}/>
             </div>
         )
     }
