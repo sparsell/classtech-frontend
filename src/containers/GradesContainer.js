11d7ace69cebafce2a76798177818cf3813ea386
Sonja Parsell <hello@sparsell.com>
2021-11-02 06:18:25 -0400

    simplifying what is rendered

--- src/containers/GradesContainer.js
+++ src/containers/GradesContainer.js
@@ -2,21 +2,20 @@ import React from 'react'
 import { connect } from 'react-redux'
 import Grades from '../components/Grades.js'
 import fetchGrades from '../actions/fetchGrades.js'
-import HomePage from '../components/HomePage.js'
+
 
 class GradesContainer extends React.Component {
 
     componentDidMount() {
             this.props.fetchGrades()
+            //  debugger
         }
 
     render() {
-        console.log("Grades Container data", this.props.grades)
+        // console.log("Grades Container data", this.props.grades)
         return (
            
             <div>
-             <HomePage />
-            GradesContainer Component
             <Grades grades={this.props.grades}/>
             </div>
         )
