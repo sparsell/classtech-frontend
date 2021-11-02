285886d2b2a7b15bf99bde560627134d2f1e0bbb
Sonja Parsell <hello@sparsell.com>
2021-11-02 06:14:15 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import HomePage from './components/HomePage.js'
 import Footer from './components/Footer'
+import Grades from './components/Grades'
+import GradeCard from './components/GradeCard'
+import NewStudentForm from './components/NewStudentForm'
 
 class App extends React.Component {
 
@@ -13,6 +16,9 @@ class App extends React.Component {
                 <div>
                     <div className="App">
                          <Route exact path='/' render={() => <HomePage />} />
+                         <Route exact path='/gradesearch' render={() => <Grades />} />
+                         <Route exact path='/students/new' render={() => <NewStudentForm />} />
+                         <Route exact path='/grades/:id/students' render={(routeProps) => <GradeCard {...routeProps}/>} />
                          <Footer />
                     </div>
                 </div>
