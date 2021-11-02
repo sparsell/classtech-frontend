7368b369bbd820d21e854e5bed1f8876fdac0a3a
Sonja Parsell <hello@sparsell.com>
2021-11-02 06:16:14 -0400

    copy edits

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,7 @@
 const Intro = () => {
     return (
         <div className="intro">
-          <h3>We know...everyone in your class SAYS they have a phone but do they {<em>really</em>}? How can you tell? </h3>
+          <p>We know...everyone in your class SAYS they have a phone but do they {<em>really</em>}?</p>
         </div>
     )
 }
