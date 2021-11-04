02cbd738e8dbe0f0b2e93ce037514ad512e176dc
Sonja Parsell <hello@sparsell.com>
2021-11-04 09:27:13 -0400

    added StudentCard return

--- src/components/GradeCard.js
+++ src/components/GradeCard.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { Link } from 'react-router-dom';
+import StudentCard from '../components/StudentCard'
 
 const GradeCard = (props) => {
 // debugger
     const grade = props.grades.find(grade => grade.id === parseInt(props.match.params.id))
 console.log(grade.students)
+ 
+//  if !==grade, fetchGrade
 
     return (
-        <div>
-            <h2>Students in Grade {grade.grade_name} </h2>
-            
-              {grade.students.map(student => <ul key={student.id}>{student.name}</ul>)}
+        <div className="grade-card">
+            <h2>Students in {grade.grade_name} </h2>  
+
+            <ul>
+                {grade.students.map(student => {
+                    return <StudentCard student={student} key={student.id} />
+                })}
+            </ul>
+
+{/* 
+              {grade.students.map(student => <ul key={student.id}>{student.name}</ul>)} */}
+
+              <Link to="/" className="ui primary button"> Home </Link>
+              <Link to="/gradesearch" className="ui primary button"> View by Grade </Link>
+              <Link to="/students/new" className="ui primary button"> Add a Student </Link>
         </div>
     )
 }
