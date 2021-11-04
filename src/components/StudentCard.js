3834436951f2d51f4d2df2e1cea57fb0d4622389
Sonja Parsell <hello@sparsell.com>
2021-11-04 09:29:56 -0400

    return styles and data rendering

--- src/components/StudentCard.js
+++ src/components/StudentCard.js
@@ -1,25 +1,32 @@
 import React from 'react'
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon} from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
-const StudentCard = () => (
-  <Card>
-    <Image src='https://react.semantic-ui.com/images/avatar/large/matthew.png' wrapped ui={false} />
-    <Card.Content>
-      <Card.Header>Matthew</Card.Header>
-      <Card.Meta>
-        <span className='date'>Joined in 2015</span>
-      </Card.Meta>
-      <Card.Description>
-        Matthew is a musician living in Nashville.
-      </Card.Description>
-    </Card.Content>
-    <Card.Content extra>
-      <a>
-        <Icon name='user' />
-        22 Friends
-      </a>
-    </Card.Content>
-  </Card>
-)
+const StudentCard = (props) => {
+    
+    const student = props.student
+    console.log(student)
+    return (
+        <div className="card-container"
+        style={{
+          width: "50%",
+          border: "solid 3px #d3d3d3",
+          margin: "10px auto"
+        }}
+        >   <div>
+                <p>{student.name}</p>
+                <p>{student.school} School</p>
+                <p>Has a phone? {student.has_phone.value}</p>
+                <p>Has a different device?{student.has_other_device}</p>
+                <p>{student.screen_time}</p>
+            </div>
+
+        
+
+        </div>
+    )
+//    check if has_phone = true, x if has_phone = false
+    
+}
 
 export default StudentCard
