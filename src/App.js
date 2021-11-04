72065eced9b933e16020f92151c676651832ef77
Sonja Parsell <hello@sparsell.com>
2021-11-04 09:25:58 -0400

    added route for GradesContainer

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { connect } from 'react-redux'
 import { Route } from 'react-router-dom'
 import HomePage from './components/HomePage.js'
 import Footer from './components/Footer'
-import Grades from './components/Grades'
 import GradeCard from './components/GradeCard'
 import NewStudentForm from './components/NewStudentForm'
+import GradesContainer from './containers/GradesContainer';
+import StudentCard from './components/StudentCard'
 
 class App extends React.Component {
 
@@ -16,8 +17,9 @@ class App extends React.Component {
                 <div>
                     <div className="App">
                          <Route exact path='/' render={() => <HomePage />} />
-                         <Route exact path='/gradesearch' render={() => <Grades />} />
-                         <Route exact path='/students/new' render={() => <NewStudentForm />} />
+                         <Route exact path='/gradesearch' render={() => <GradesContainer />} />
+                         <Route exact path='/students/new' render={(routeProps) => <NewStudentForm {...routeProps}/>} />
+                         <Route exact path="/student/:id" render={(routeProps) => <StudentCard {...routeProps}/>} />
                          <Route exact path='/grades/:id/students' render={(routeProps) => <GradeCard {...routeProps}/>} />
                          <Footer />
                     </div>
