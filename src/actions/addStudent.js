c3ef049946fa360744e1cd5e6bc0ab2b48516f0e
Sonja Parsell <hello@sparsell.com>
2021-11-04 09:26:55 -0400

    added .then to post new student

--- src/actions/addStudent.js
+++ src/actions/addStudent.js
@@ -1,5 +1,5 @@
 
-export const addStudent = (data) => {
+export const addStudent = (data, history) => {
     return (dispatch) => {
       fetch('http://localhost:3000/api/v1/students', {
         headers: {
@@ -8,6 +8,11 @@ export const addStudent = (data) => {
         },
         method: 'POST',
         body: JSON.stringify({student: data})
+      })
+      .then(resp => resp.json())
+      .then(data => {
+          console.log(data)
+        history.push(`student/${data.data.attributes.id}`)
       })
     }
 }
