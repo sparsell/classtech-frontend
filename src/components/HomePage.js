e09aeb0dea40710a04bdf1e6eadffc7d94f9b7dc
Sonja Parsell <hello@sparsell.com>
2021-11-04 09:28:25 -0400

    removed unnecessary imports

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,18 @@
 import React from 'react'
 import Greeting from '../components/Greeting'
 import Intro from '../components/Intro'
+import LandingPageImage from '../components/LandingPageImage'
 import NewStudentButton from '../components/NewStudentButton'
 import ShowResultsButton from '../components/ShowResultsButton'
-import LandingPageImage from '../components/LandingPageImage'
-import SeeResultsForm from './SeeResultsForm'
-import NewStudentForm from './NewStudentForm'
-import GradesContainer from '../containers/GradesContainer'
-import StudentsContainer from '../containers/StudentsContainer'
-
 
 const HomePage = () => {
     return (
         <div>
             <div>
                 <Greeting />
-            </div>
-
-             <h1> Who has a phone in your grade?</h1>
-
-            <div>
+                </div>
+                <h1> Who has a phone in your grade?</h1>
+                <div>
                 <LandingPageImage />
                 <Intro />
             </div>
@@ -28,20 +21,10 @@ const HomePage = () => {
             <br></br>
 
             <div>
-            <GradesContainer />
-                <br></br>
-                <br></br>
-                {/* <ShowResultsButton /> */}
-
-               
+                <ShowResultsButton />
                 <h3> -- OR -- </h3>
-                <NewStudentButton />
-                {/* <NewStudentForm /> */}
-                {/* <StudentsContainer /> */}
-                
+                <NewStudentButton />   
             </div>
-
-           
         </div>
     )
 }
