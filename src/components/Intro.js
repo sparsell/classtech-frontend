e6e13563af8d3e90686b0fccc0d35c98ac131441
Sonja Parsell <hello@sparsell.com>
2021-11-08 09:34:31 -0500

    improved readability of intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,7 +3,8 @@
 const Intro = () => {
     return (
         <div className="intro">
-          <h3>We know...everyone in your class SAYS they have a phone but do they {<em>really</em>}? Find out below. </h3>
+          <h3>We know...everyone in your class SAYS they have a phone but do they {<em>really</em>}? </h3>
+          <h3>Find out below. </h3>
         </div>
     )
 }
