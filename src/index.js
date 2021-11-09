ea5b2541a0339fc8d48210b5c91d93b2ce4e241c
Sonja Parsell <hello@sparsell.com>
2021-11-09 08:57:25 -0500

    wip - using rootReducer

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { BrowserRouter as Router} from 'react-router-dom';
 import { Provider } from 'react-redux'
 import gradeReducer from './reducers/gradeReducer'
-import reportWebVitals from './reportWebVitals';
+import studentReducer from './reducers/studentReducer'
 
 import App from './App';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const rootReducer = combineReducers({
+    studentReducer, 
+    gradeReducer
+})
 
 const store = createStore(
     gradeReducer,
@@ -28,4 +33,3 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-reportWebVitals();
