206d07f605a230abcf6b7d38941596aaca623cad
Sonja Parsell <hello@sparsell.com>
2021-11-09 08:56:00 -0500

    fixed className error (only had class)

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 const Greeting = () => {
     return (
-        <container>
-            <div class="ui center aligned container" id="greeting">
+        
+            <div className="ui center aligned container" id="greeting">
                 <h1 className="greeting-header">ClassTech</h1>
                     <p className="tagline">"You see darling, not everyone in your class has a phone."</p>
                     <p><em>- every parent</em></p>
             </div>
-        </container>
+       
     )
 }
 
