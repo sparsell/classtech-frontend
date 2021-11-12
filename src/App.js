b156ce9c9c8d2a6b3b21319128e70f28bc247f1b
Sonja Parsell <hello@sparsell.com>
2021-11-12 08:48:38 -0500

    using BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import './index.css'
 import React from 'react'
 import { connect } from 'react-redux'
-import { Route } from 'react-router-dom'
+import { BrowserRouter, Route } from 'react-router-dom'
 import HomePage from './components/HomePage.js'
 import GradeCard from './components/GradeCard'
 import NewStudentForm from './components/NewStudentForm'
@@ -13,10 +13,14 @@ class App extends React.Component {
     render() {
         return (
             <div className="App">
-                <Route exact path='/' render={() => <HomePage />} />
-                <Route exact path='/grades' render={(routeProps) => <GradesContainer {...routeProps}/>} />
-                <Route exact path='/students/new' render={(routeProps) => <NewStudentForm {...routeProps}/>} />
-                <Route exact path='/grades/:id/students' render={(routeProps) => <GradeCard {...routeProps}/>} />
+                <BrowserRouter>
+                    <div>
+                        <Route exact path='/' render={() => <HomePage />} />
+                        <Route exact path='/grades' render={(routeProps) => <GradesContainer {...routeProps}/>} />
+                        <Route exact path='/students/new' render={(routeProps) => <NewStudentForm {...routeProps}/>} />
+                        <Route exact path='/grades/:id/students' render={(routeProps) => <GradeCard {...routeProps}/>} />
+                    </div>
+                </BrowserRouter>
             </div>
         ) 
     }
