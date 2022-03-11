e9017ebbcd6d025edc00c6d8e55e338315527d70
sparsell <hello@sparsell.com>
2022-03-11 17:11:14 -0500

    adding segment

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,31 +1,27 @@
-import React from 'react'
-import Greeting from '../components/Greeting'
-import Intro from '../components/Intro'
-import LandingPageImage from '../components/LandingPageImage'
-import NewStudentButton from '../components/NewStudentButton'
-import ShowResultsButton from '../components/ShowResultsButton'
+import React from 'react';
+import Greeting from '../components/Greeting';
+import Intro from '../components/Intro';
+import OptionView from '../components/OptionView';
+import LandingPageImage from '../components/LandingPageImage';
+import NewStudentButton from '../components/NewStudentButton';
+import ShowResultsButton from '../components/ShowResultsButton';
+import { Segment, Header, Image, Container } from 'semantic-ui-react';
 
 const HomePage = () => {
     return (
-        <div>
-            <div>
-                <Greeting />
-                    </div>
-                        <h1 className="greeting"> Who has a phone in your grade?</h1>
-                    <div>
-                <LandingPageImage />
-                <Intro />
-            </div>
-
-            <br></br>
-            <br></br>
+        <Segment className="cover">
+                    <Greeting />
+                    <LandingPageImage />
+                    <Intro />
 
             <div className="homepage-buttons">
                 <ShowResultsButton />
                 <h3> -- OR -- </h3>
                 <NewStudentButton />   
-            </div>
-        </div>
+             </div>
+             <OptionView />
+        </Segment>
+      
     )
 }
 
